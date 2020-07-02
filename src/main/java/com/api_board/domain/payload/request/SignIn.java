10ecdd5d4c233494d2e740e36223f3a05e714ea6
Hanif <marbling1293@naver.com>
2020-07-03 08:39:10 +0900

    [Modify] request

--- src/main/java/com/api_board/domain/payload/request/SignIn.java
+++ src/main/java/com/api_board/domain/payload/request/SignIn.java
@@ -1,13 +1,17 @@
 package com.api_board.domain.payload.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignIn {
 
     @Email
