07c31a6cf6ac30f92080bcef755b6e9d08932e86
Hanif <marbling1293@naver.com>
2020-08-07 09:44:04 +0900

    [FIX]

--- src/main/java/com/api_board/domain/payload/request/ChargePasswordRequest.java
+++ src/main/java/com/api_board/domain/payload/request/ChargePasswordRequest.java
@@ -2,11 +2,14 @@ package com.api_board.domain.payload.request;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class ChargePasswordRequest {
 
+    @NotBlank
     private String password;
 }
--- src/main/java/com/api_board/domain/payload/response/FileUploadResponse.java
+++ src/main/java/com/api_board/domain/payload/response/FileUploadResponse.java
@@ -10,7 +10,5 @@ import lombok.Setter;
 public class FileUploadResponse {
 
     private String fileName;
-    private String fileDownloadUri;
-    private String fileType;
-    private long size;
+
 }
