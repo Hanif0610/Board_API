9da3fbe81b84bb938b2e74555fa5355679836237
Hanif <marbling1293@naver.com>
2020-08-10 22:07:07 +0900

    [ADD] comment exception

---
+++ src/main/java/com/api_board/exception/CommentNotFoundException.java
@@ -0,0 +1,11 @@
+package com.api_board.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class CommentNotFoundException extends RuntimeException {
+    public CommentNotFoundException() {
+        super("Comment Not Found");
+    }
+}
